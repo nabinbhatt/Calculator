d24327357721882ac0651f28b31bc5fc4fb21fa4
Nabin Bhatt <info@bhattnabin.com.np>
2021-12-26 18:11:23 +0545

    updated service worker

--- sw.js
+++ sw.js
@@ -1,18 +1,61 @@
-// This is the service worker with the Cache-first network
+var APP_PREFIX = "NB_";
+var VERSION = "calc_v1";
+var CACHE_NAME = APP_PREFIX + VERSION;
+var URLS = [
+  "/",
+  "/?utm_source=pwa_install",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/favicon.ico"
+];
 
-const CACHE = "pwabuilder-precache";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// Respond with cached resources
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        // if cache is available, respond with cache
+        console.log("responding with cache : " + e.request.url);
+        return request;
+      } else {
+        // if there are no cache, try fetching request
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
+      }
+    })
+  );
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.CacheFirst({
-    cacheName: CACHE
-  })
-);
+// Cache resources
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(URLS);
+    })
+  );
+});
+
+// Delete outdated caches
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
